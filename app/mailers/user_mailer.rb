2c11df8e5b0052a548891aff62ea847d0cabf188
ohdDev <w.ohd95@gmail.com>
2022-02-12 22:36:34 +0300

    create Item model

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+    def welcome_email
+        @user = params[:user]
+        mail(to: "ohoud@email.com", subject: "Welcome email!")
+    end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-    def welcome_mail
-        user = User.new(name: 'Mohamed ', email: 'mohamed@example.com' , password: '123' , login: '2022')
-        
-        UserMailer.with(user: user).welcome_mail
+    def welcome_email
+        user = User.new(name: 'Mohamed ', email: 'mohamed@example.com')
+
+        UserMailer.with(user: user).welcome_email
 
     end
 
