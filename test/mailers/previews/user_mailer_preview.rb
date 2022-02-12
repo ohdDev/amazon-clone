7b7673d17aa70ec47cbbebbf4c1617e3025c6210
ohdDev <w.ohd95@gmail.com>
2022-02-12 21:26:52 +0300

    create mailer views and preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+    def welcome_mail
+        user = User.new(name: 'Mohamed ', email: 'mohamed@example.com' , password: '123' , login: '2022')
+        
+        UserMailer.with(user: user).welcome_mail
+
+    end
 
 end
