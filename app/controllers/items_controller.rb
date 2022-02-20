a938a8720e5b432c1e67268dcb0c465812111de9
ohdDev <w.ohd95@gmail.com>
2022-02-20 07:49:49 +0300

    create active storage and add it to the form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -72,6 +72,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :description, :price)
+      params.require(:item).permit(:name, :description, :price, :picture)
     end
 end
