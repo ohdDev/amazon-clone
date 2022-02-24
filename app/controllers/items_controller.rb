c1d4957e38e844a0792e1ca46ddc2c60384b7496
ohdDev <w.ohd95@gmail.com>
2022-02-24 04:42:21 +0300

    delete picture

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: %i[ show edit update destroy ]
+  before_action :set_item, only: %i[ show edit update destroy  ]
 
   # GET /items or /items.json
   def index
@@ -8,6 +8,12 @@ class ItemsController < ApplicationController
 
   # GET /items/1 or /items/1.json
   def show
+  end
+
+  def purge_picture
+    @item = Item.find(params[:id])
+    @item.picture.purge
+    redirect_back fallback_location: items_path, notice: "success"
   end
 
   # GET /items/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :items
+  resources :items do
+    member do
+      delete :purge_picture
+    end
+  end
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/items/:id/purge_picture', to: 'items#purge_picture', as: :DELETE
+
 end
